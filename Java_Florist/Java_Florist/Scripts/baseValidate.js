2564660f299cb8a0eb47da1029427331995f4e7e
Nguyen Ngoc Tien <64886876+tientulac@users.noreply.github.com>
2022-10-07 11:33:41 +0700

    add validator

---
+++ Java_Florist/Java_Florist/Scripts/baseValidate.js
@@ -0,0 +1,10 @@
+﻿validateForm = (id_element) => {
+    var value = $('#' + id_element).val();
+    if (!(value.length > 0)) {
+        $('#' + id_element + '_validate').css('display', 'block');
+        $('#' + id_element + '_validate').html(`Field ${id_element} is required !`);
+    }
+    else {
+        $('#' + id_element + '_validate').css('display', 'none');
+    }
+}
